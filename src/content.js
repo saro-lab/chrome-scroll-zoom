73126bc63d9f359b1803c8933faf85e9caece7b0
marker <j@saro.me>
2025-01-18 22:31:43 +0900

    v1

--- src/content.js
+++ src/content.js
@@ -1,32 +1,27 @@
 let zoom = {
-  get active() {
-    return (zoom.keyTime + 500) > new Date().getTime();
-  },
-  key: "Meta",
-  keyTime: 0,
+  active: false,
+  eventKey: "Meta",
   unit: 0.05,
+  direction: 1,
   useScroll: true,
   useReset: true,
 };
 
-document.addEventListener("keypress", (event) => {
-  if (event.key === zoom.eventKey) {
-    zoom.keyTime = new Date().getTime();
-  }
-});
+// activate
+document.addEventListener("keydown", (event) => zoom.active = (event.key === zoom.eventKey));
 
-document.addEventListener("keyup", (event) => {
-  if (event.key === zoom.eventKey) {
-    zoom.keyTime = 0;
-  }
-});
+// deactivate
+document.addEventListener("keyup", (event) => zoom.active = false);
+document.addEventListener("blur", (event) => zoom.active = false);
 
+// zoom in/out
 document.addEventListener("wheel", (event) => {
   if (zoom.active && zoom.useScroll) {
-    chrome.runtime.sendMessage({action: "zoom", zoom: event.deltaY > 0 ? -zoom.unit : zoom.unit});
+    chrome.runtime.sendMessage({action: "zoom", zoom: (event.deltaY < 0 ? -zoom.unit : zoom.unit) * zoom.direction});
   }
 });
 
+// reset zoom
 window.addEventListener('mousedown', (event) => {
   if (zoom.active && zoom.useReset && event.button === 1) {
     try { event.preventDefault(); } catch (e) {}
